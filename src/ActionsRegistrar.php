b3c0c081704a481601139171a45546e823b3611f
daniel21703 <daniel217032001@gmail.com>
2025-02-12 23:26:46 +0300

    Add ActionsRegistrar

---
+++ src/ActionsRegistrar.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnePix\WordPressContractsImplementation;
+
+final class ActionsRegistrar implements \OnePix\WordPressContracts\ActionsRegistrar
+{
+    public function add(string $hook, callable $callback, int $priority = 10, int $acceptedArgs = 1): void
+    {
+        add_action($hook, $callback, $priority, $acceptedArgs);
+    }
+
+    public function remove(string $hook, callable $callback, int $priority = 10): void
+    {
+        remove_action($hook, $callback, $priority);
+    }
+
+    public function has(string $hook, callable $callback): bool
+    {
+        return has_action($hook, $callback);
+    }
+
+    public function hasAny(string $hook): bool
+    {
+        return has_action($hook);
+    }
+}
