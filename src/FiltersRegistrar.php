3b214ebc3fc44e9dc118245c7da371f19cf252f1
daniel21703 <daniel217032001@gmail.com>
2025-02-13 00:18:19 +0300

    Add FiltersRegistrar

---
+++ src/FiltersRegistrar.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnePix\WordPressContractsImplementation;
+
+final class FiltersRegistrar implements \OnePix\WordPressContracts\FiltersRegistrar
+{
+    public function add(string $hook, callable $callback, int $priority = 10, int $acceptedArgs = 1): void
+    {
+        /**
+         * @psalm-suppress ArgumentTypeCoercion
+         * @psalm-suppress PossiblyInvalidArgument
+         */
+        add_filter($hook, $callback, $priority, $acceptedArgs);
+    }
+
+    public function remove(string $hook, callable $callback, int $priority = 10): void
+    {
+        remove_filter($hook, $callback, $priority);
+    }
+
+    public function has(string $hook, callable $callback): bool
+    {
+        return has_filter($hook, $callback);
+    }
+
+    public function hasAny(string $hook): bool
+    {
+        return has_filter($hook);
+    }
+}
