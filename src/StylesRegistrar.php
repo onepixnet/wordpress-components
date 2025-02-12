16c876397f372cd041d37993713b15f63ed40ff5
daniel21703 <daniel217032001@gmail.com>
2025-02-13 00:57:54 +0300

    Add StylesRegistrar

---
+++ src/StylesRegistrar.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnePix\WordPressContractsImplementation;
+
+use OnePix\WordPressContracts\Style;
+
+final class StylesRegistrar implements \OnePix\WordPressContracts\StylesRegistrar
+{
+    public function registerStyle(Style $style): bool
+    {
+        return wp_register_style(
+            $style->getHandle(),
+            $style->getSrc(),
+            $style->getDeps(),
+            $style->getVersion(),
+            $style->getMedia()
+        );
+    }
+
+    public function enqueueStyle(Style $style): bool
+    {
+        wp_enqueue_style($style->getHandle());
+        return true;
+    }
+
+    public function deregisterStyle(Style $style): bool
+    {
+        wp_deregister_style($style->getHandle());
+        return true;
+    }
+
+    public function dequeueStyle(Style $style): bool
+    {
+        wp_dequeue_style($style->getHandle());
+        return true;
+    }
+}
