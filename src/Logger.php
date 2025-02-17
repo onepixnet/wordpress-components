8d7bc6ddc8933d1cae72fedf1759a524544c9452
daniel21703 <daniel217032001@gmail.com>
2025-02-17 18:15:43 +0300

    Update Logger

--- src/Logger.php
+++ src/Logger.php
@@ -10,68 +10,63 @@ use Psr\Log\LogLevel;
 class Logger implements LoggerInterface
 {
     public function __construct(
-        private readonly string $plugin_id
+        private readonly string $appPrefix
     ) {
     }
 
-    public function emergency($message, array $context = []): void
+    public function emergency(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert($message, array $context = []): void
+    public function alert(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = []): void
+    public function critical(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error($message, array $context = []): void
+    public function error(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning($message, array $context = []): void
+    public function warning(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice($message, array $context = []): void
+    public function notice(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info($message, array $context = []): void
+    public function info(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::INFO, $message, $context);
     }
 
-    public function debug($message, array $context = []): void
+    public function debug(mixed $message, array $context = []): void
     {
         $this->log(LogLevel::DEBUG, $message, $context);
     }
 
-    /**
-     * @param $level
-     * @param $message
-     *
-     */
-    public function log($level, $message, array $context = []): void
+    public function log(mixed $level, mixed $message, array $context = []): void
     {
         if (empty($context['codeSource'])) {
-            $backtrace              = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1];
+            $backtrace              = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1];//phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_debug_backtrace
             $context['codeSource']  = isset($backtrace['class']) ? $backtrace['class'] . '::' : '';
             $context['codeSource'] .= $backtrace['function'] ?? '';
         }
 
         $data = [
-            'source' => "{$this->plugin_id}: {$context['codeSource']}",
+            'source' => "{$this->appPrefix}: {$context['codeSource']}",
             'data'   => $message,
         ];
 
-        error_log(print_r($data, true));
+        error_log(print_r($data, true));//phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r
     }
 }
