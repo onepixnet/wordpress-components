97e8921e5543c54811ea6908d0e95be1abb24abb
daniel21703 <daniel217032001@gmail.com>
2025-02-17 17:34:23 +0300

    Add RewriteRulesManager

---
+++ src/RewriteRulesManager.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnePix\WordPressContractsImplementation;
+
+final class RewriteRulesManager implements \OnePix\WordPressContracts\RewriteRulesManager
+{
+    public function __construct(
+        private readonly string $optionPrefix
+    )
+    {
+    }
+
+    public function flushRewriteRules(bool $disablingPlugin = false): void
+    {
+        if ($disablingPlugin) {
+            flush_rewrite_rules(false);
+            $this->setRewriteRulesFlashed(false);
+        } elseif (! $this->wasRewriteRulesFlashed()) {
+            flush_rewrite_rules(false);
+            $this->setRewriteRulesFlashed(true);
+        }
+    }
+
+    private function setRewriteRulesFlashed(bool $value): void
+    {
+        update_option(
+            $this->getRewriteRulesFlushedOptionKey(),
+            $value ? '1' : '0'
+        );
+    }
+
+    private function wasRewriteRulesFlashed(): bool
+    {
+        return get_option($this->getRewriteRulesFlushedOptionKey()) === '1';
+    }
+
+    private function getRewriteRulesFlushedOptionKey(): string
+    {
+        return $this->optionPrefix . '_rewrite_rules_flushed';
+    }
+}
